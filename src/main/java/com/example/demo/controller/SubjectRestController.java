5b9c654ee2fd200b0a735ec643d90615be57da53
whitepoem12 <whitepoem12@gmail.com>
2024-10-28 15:14:03 +0900

    사번을 리턴하는 API 추가 1. 사번 하드코딩 2. 입력한 사번 그대로 리턴

---
+++ src/main/java/com/example/demo/controller/SubjectRestController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/v1/user")
+@RestController
+public class SubjectRestController {
+    /**
+     * 하드코딩 사번 리턴
+     * @return 82265640
+     */
+    @GetMapping
+    public String getUserNumber(){
+        return "82265640";
+    }
+
+    /**
+     * 입력된 사번 리턴
+     * @param userNumber
+     * @return {userNumber}
+     */
+    @GetMapping("/{userNumber}")
+    public String getUserName(@PathVariable("userNumber") String userNumber){
+        return userNumber;
+    }
+}
